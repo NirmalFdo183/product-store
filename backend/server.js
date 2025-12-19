a921dc9f7dcc95fe25c561d6090d79c664e83893
Nirmal <fernandonirmal345@gmail.com>
2025-12-19 21:44:47 +0530

    server setup changes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 
 const app = express();
+const port = 4000;
 
-app.listen(5000,()=>{
-    console.log("server is listened on the port 5000.")
+app.listen(4000,()=>{
+    console.log(`server is listened on the ${port}. http://localhost:${port}`);
 })
