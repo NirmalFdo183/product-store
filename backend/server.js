6791840b07f0eb6dc8d3f196664565323fe1b1dd
Nirmal <fernandonirmal345@gmail.com>
2025-12-19 22:23:46 +0530

    setup the database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 const port = 4000;
 
 app.listen(4000,()=>{
+    connectDB();
     console.log(`server is listened on the ${port}. http://localhost:${port}`);
 })
